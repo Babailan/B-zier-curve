814eb1c1d2b40c6266b9e039d37d284478f95bb8
Babailan <ronelbabailan@hotmail.com>
2022-02-10 23:55:25 +0800

    MEshes

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ class Frick {
     const canvas = document.querySelector("#c");
     this.Renderer = new THREE.WebGL1Renderer({ canvas });
     this.Scene = new THREE.Scene();
+    this.Meshes = {};
     this.size = {
       width: window.innerWidth,
       height: window.innerHeight,
@@ -22,44 +23,30 @@ class Frick {
 
     this.init();
   }
+  init() {
+    this.axesHelper();
+    this.cameraChanges();
+    this.Plane();
+    this.Box();
+    this.Sphere();
+    this.spotLight();
+    this.rendererConfig();
+
+    //Event Listener
+    this.onResize();
+  }
+  rendererConfig() {
+    this.Renderer.shadowMap.enabled = true;
+    this.Renderer.setPixelRatio(window.devicePixelRatio);
+    this.Renderer.setClearColor(0x000000);
+    this.Renderer.setSize(this.size.width, this.size.height);
+    this.Renderer.render(this.Scene, this.Camera);
+  }
   cameraChanges() {
-    this.Camera.position.set(-30, 40, 40);
+    this.Camera.position.set(-30, 30, 30);
     this.Camera.lookAt(this.Scene.position);
   }
-  axesHelper() {
-    const axes = new THREE.AxesHelper(20);
-    this.Scene.add(axes);
-  }
-  Plane() {
-    const planeGeometry = new THREE.PlaneGeometry(60, 30);
-    const planeMaterial = new THREE.MeshBasicMaterial({
-      color: 0x808080,
-      side: THREE.DoubleSide,
-    });
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.position.y = -3.5;
-    plane.rotateX(Math.PI * -0.5);
-    this.Scene.add(plane);
-  }
-  Box() {
-    const boxGeometry = new THREE.BoxGeometry(4, 4, 4);
-    const materialBox = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-    });
-    const Box = new THREE.Mesh(boxGeometry, materialBox);
-    Box.position.x = -10;
-    this.Scene.add(Box);
-  }
-  spotLight() {
-    const spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-40, 40, -15);
-    spotLight.castShadow = true;
-    spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
-    console.log(spotLight.shadow.mapSize);
-    spotLight.shadow.camera.far = 130;
-    spotLight.shadow.camera.near = 40;
-    this.Scene.add(spotLight);
-  }
+
   onResize() {
     window.addEventListener("resize", () => {
       this.size.width = window.innerWidth;
@@ -72,18 +59,58 @@ class Frick {
       this.Renderer.render(this.Scene, this.Camera);
     });
   }
-  init() {
-    this.axesHelper();
-    this.cameraChanges();
-    this.Plane();
-    this.Box();
-    this.Renderer.setPixelRatio(window.devicePixelRatio);
-    this.Renderer.setSize(this.size.width, this.size.height);
-    this.Renderer.render(this.Scene, this.Camera);
 
-    //Event Listener
-    this.onResize();
+  //geometries
+  axesHelper() {
+    const axes = new THREE.AxesHelper(12);
+    this.Scene.add(axes);
+  }
+  Plane() {
+    const planeGeometry = new THREE.PlaneGeometry(60, 30);
+    const planeMaterial = new THREE.MeshLambertMaterial({
+      color: 0x808080,
+      side: THREE.DoubleSide,
+    });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.position.y = -3.5;
+    plane.rotateX(Math.PI * -0.5);
+    plane.receiveShadow = true;
+    this.Meshes["plane"] = { ...plane };
+    this.Scene.add(plane);
+  }
+  Box() {
+    const boxGeometry = new THREE.BoxGeometry(4, 4, 4);
+    const materialBox = new THREE.MeshLambertMaterial({
+      color: 0xff0000,
+    });
+    const Box = new THREE.Mesh(boxGeometry, materialBox);
+    Box.position.x = -10;
+    Box.castShadow = true;
+    this.Scene.add(Box);
+  }
+  spotLight() {
+    const spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-50, 30, 10);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+    spotLight.shadow.camera.far = 130;
+    spotLight.shadow.camera.near = 4;
+    spotLight.visible = true;
+    this.Scene.add(spotLight);
+  }
+  Sphere() {
+    const sphereGeometry = new THREE.SphereGeometry(4);
+    const material = new THREE.MeshLambertMaterial({ color: 0x0000ff });
+    const mesh = new THREE.Mesh(sphereGeometry, material);
+    mesh.position.x = 20;
+    mesh.castShadow = true;
+    this.Scene.add(mesh);
+  }
+
+  animate() {
+    this.rendererConfig();
   }
 }
 
-new Frick();
+const Render = new Frick();
+Render.animate();
