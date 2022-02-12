8c1fefba336d6f7601d99036f3f0851cc3896c58
Babailan <ronelbabailan@hotmail.com>
2022-02-12 22:42:56 +0800

    stats with gui

--- src/components/picture.js
+++
@@ -1,9 +0,0 @@
-import Layunin from "../../assets/Layunin.jpg";
-
-function component() {
-  const tag = document.createElement("div");
-  tag.classList.add("image");
-  return tag;
-}
-
-export default component;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import * as THREE from "three";
 import "./style/style.css";
 import Stats from "stats.js";
+import { TrackballControls } from "three/examples/jsm/controls/TrackballControls";
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// import * as dat from "dat.gui";
+import * as dat from "dat.gui";
 
-class Frick {
+class Parent {
   constructor() {
     //properties
     this.canvas = document.querySelector("#c");
@@ -25,14 +26,18 @@ class Frick {
       0.1,
       1000
     );
+    this.trackball = this.cameraChanges();
     //stats
     this.stats = this.statsPanel();
     //otherProperties
     this.steps = 0;
+    this.guiProperties = {
+      bounceSpeed: 0.04,
+      boxRotation: 0.04,
+    };
     this.init();
   }
   init() {
-    this.cameraChanges();
     this.spotLight();
     this.rendererConfig();
     this.axesHelper();
@@ -40,6 +45,7 @@ class Frick {
     this.onResize();
     this.boxAnimation();
     this.sphereAnimation();
+    this.gui();
     this.Rerender();
   }
   rendererConfig() {
@@ -49,10 +55,12 @@ class Frick {
     this.Renderer.setSize(this.size.width, this.size.height);
   }
   cameraChanges() {
-    this.Camera.position.set(0, 0, 30);
+    this.Camera.position.z = 0.1;
     this.Camera.lookAt(this.Scene.position);
+    const trackBall = new TrackballControls(this.Camera, this.canvas);
+    const clock = new THREE.Clock();
+    return { trackBall, clock };
   }
-
   onResize() {
     window.addEventListener("resize", () => {
       this.size.width = window.innerWidth;
@@ -65,11 +73,10 @@ class Frick {
       this.Renderer.setPixelRatio(window.devicePixelRatio);
     });
   }
-
   // lights
   spotLight() {
     const spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(-200, 100, 30);
+    spotLight.position.set(-20, 50, 30);
     spotLight.castShadow = true;
     spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
     spotLight.shadow.camera.far = 130;
@@ -83,7 +90,7 @@ class Frick {
     this.Scene.add(axes);
   }
   Plane() {
-    const planeGeometry = new THREE.PlaneGeometry(60, 30);
+    const planeGeometry = new THREE.PlaneGeometry(70, 30);
     const planeMaterial = new THREE.MeshLambertMaterial({
       color: 0x808080,
       side: THREE.DoubleSide,
@@ -92,7 +99,7 @@ class Frick {
     mesh.position.y = -3.5;
     mesh.rotateX(Math.PI * -0.5);
     mesh.receiveShadow = true;
-    // this.Scene.add(mesh);
+    this.Scene.add(mesh);
     return mesh;
   }
   Box() {
@@ -103,7 +110,7 @@ class Frick {
     const mesh = new THREE.Mesh(boxGeometry, materialBox);
     mesh.position.x = -10;
     mesh.castShadow = true;
-    // this.Scene.add(mesh);
+    this.Scene.add(mesh);
     return mesh;
   }
   Sphere() {
@@ -115,33 +122,40 @@ class Frick {
     this.Scene.add(mesh);
     return mesh;
   }
-
   statsPanel() {
     const stats = new Stats();
-    stats.showPanel(2); // 0: fps, 1: ms, 2: mb, 3+: custom
+    stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     document.body.appendChild(stats.dom);
     return stats;
   }
+  //rerenderer
   Rerender = () => {
     this.stats.begin();
     this.stats.end();
     this.Renderer.render(this.Scene, this.Camera);
     requestAnimationFrame(this.Rerender);
+    this.trackball.trackBall.update(this.trackball.clock.getDelta());
+  };
+  // gui
+  gui = () => {
+    const gui = new dat.GUI();
+    gui.domElement.style.marginRight = "50px";
+    gui.add(this.guiProperties, "bounceSpeed", 0, 0.5);
+    gui.add(this.guiProperties, "boxRotation", 0, 0.5);
   };
   //animaiton
   boxAnimation = () => {
-    this.Meshes.box.rotation.x += 0.01;
-    this.Meshes.box.rotation.y += 0.01;
-    this.Meshes.box.rotation.z += 0.01;
+    this.Meshes.box.rotation.x += this.guiProperties.boxRotation;
+    this.Meshes.box.rotation.y += this.guiProperties.boxRotation;
+    this.Meshes.box.rotation.z += this.guiProperties.boxRotation;
     requestAnimationFrame(this.boxAnimation);
   };
   sphereAnimation = () => {
-    this.steps += 0.018;
-    this.Meshes.sphere.position.x = 10 * Math.cos(this.steps);
-    this.Meshes.sphere.position.z = 10 * Math.sin(this.steps);
+    this.Meshes.sphere.position.x = 20 + 8 * Math.cos(this.steps);
+    this.Meshes.sphere.position.y = 10 * Math.abs(Math.sin(this.steps));
+    this.steps += this.guiProperties.bounceSpeed;
     requestAnimationFrame(this.sphereAnimation);
   };
 }
-THREE.MathUtils.clamp();
 
-const Render = new Frick();
+new Parent();
