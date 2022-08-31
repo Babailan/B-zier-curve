8da8d33b7c406274c82834595a3fd881fc9ad2f8
Babailan <babailanxx@gmail.com>
2022-08-31 21:56:21 +0800

    last children and scenegraph learned

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,7 @@ if (window.innerWidth < window.innerHeight) {
         let far = 1000;
         const camera = new PerspectiveCamera(fov, window.innerWidth / window.innerHeight, near, far);
         {
-            camera.position.z = 20;
             camera.position.y = 40;
-            camera.position.x = 30;
         }
         const scene = new Scene();
 
@@ -22,9 +20,11 @@ if (window.innerWidth < window.innerHeight) {
 
         window.addEventListener("resize", () => {
             camera.aspect = window.innerWidth / window.innerHeight;
-            camera.updateProjectionMatrix()
-            renderer.setSize(window.innerWidth, window.innerHeight)
-
+            camera.updateProjectionMatrix();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            if (window.innerWidth < window.innerHeight) {
+                document.body.innerHTML += ("<h1>landscape bruh</h1>");
+            };
         })
 
         const canvas = renderer.domElement;
@@ -36,7 +36,7 @@ if (window.innerWidth < window.innerHeight) {
 
 
         {    // sun
-            const radius = 10;
+            const radius = 5;
             const widthSegments = 15;
             const heightSegments = 15;
             const sphereGeometry = new SphereGeometry(
@@ -45,17 +45,26 @@ if (window.innerWidth < window.innerHeight) {
             const sunMesh = new Mesh(sphereGeometry, sunMaterial);
             scene.add(sunMesh);
             // earth
-            const earthMaterial = new MeshPhongMaterial({ specular: 0x000000, color: 0x0000ff, shininess: 0, emissive: 152238, emissiveIntensity: 0.4, wireframe: false });
-            const earthMesh = new Mesh(sphereGeometry, earthMaterial);
-            earthMesh.position.x = sunMesh.position.x + 30;
-            earthMesh.scale.set(0.2, 0.2, 0.2);
-            // add child
-            sunMesh.add(earthMesh);
-            camera.lookAt(earthMesh.position.x, earthMesh.position.y, earthMesh.position.z);
-            earthMesh.name = "earth";
+            {
+                const earthMaterial = new MeshPhongMaterial({ specular: 0x000000, color: 0x0000ff, shininess: 0, emissive: 152238, emissiveIntensity: 0.4, wireframe: false });
+                const earthMesh = new Mesh(sphereGeometry, earthMaterial);
+                earthMesh.position.x = sunMesh.position.x + 15;
+                earthMesh.scale.set(0.2, 0.2, 0.2);
+                // add child
+                sunMesh.add(earthMesh);
+                camera.lookAt(earthMesh.position.x, earthMesh.position.y, earthMesh.position.z);
+                earthMesh.name = "earth";
+
+                {
+                    const moonMaterial = new MeshPhongMaterial({ color: 0xffffff })
+                    const moonMesh = new Mesh(sphereGeometry, moonMaterial);
+                    moonMesh.scale.set(0.2, 0.2, 0.2);
+                    moonMesh.position.x = 10
+                    earthMesh.add(moonMesh);
+                }
+                objects.push(earthMesh);
+            };
             objects.push(sunMesh);
-            objects.push(earthMesh);
-            console.log(objects)
         }
 
         const control = new OrbitControls(camera, canvas);
@@ -68,21 +77,28 @@ if (window.innerWidth < window.innerHeight) {
             const intensity = 1;
             const light = new PointLight(color, intensity);
             scene.add(light);
-            const axesHelper = new AxesHelper(1);
-            axesHelper.scale.set(3, 3, 3);
-            axesHelper.position.y = 20
-            scene.add(axesHelper);
+            // const axesHelper = new AxesHelper(1);
+            // axesHelper.scale.set(3, 3, 3);
+            // axesHelper.position.y = 20
+            // scene.add(axesHelper);
         }
-
+        const times = 0.006;
         function animate() {
             requestAnimationFrame(animate)
             control.update();
+
             objects.map((v) => {
+
                 if (v.name !== "earth") {
-                    v.rotation.y += 0.006;
+                    v.rotation.y += times;
                     if (v.rotation.y > Math.PI * 2) {
                         v.rotation.y %= (Math.PI * 2);
                     };
+                } else {
+                    if (v.rotation.z > Math.PI * 2) {
+                        v.rotation.z = v.rotation.z % (Math.PI * 2);
+                    }
+                    v.rotation.z += (times * 5);
                 }
 
             })
