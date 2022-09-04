a9c530939135fcbefb71073a64567277ff331f0d
Babailan <babailanxx@gmail.com>
2022-09-04 14:03:22 +0800

    quadratic bezier curve

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
 import "./style/style.css";
-import { btw_01, lerp2points } from './lerp'
+import { btw_01, quadraticBezierCurve, drawLine } from './lerp';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
 function main() {
-    const canvas = document.querySelector("#c");
+    const canvas = document.querySelector("#c") as HTMLCanvasElement;
     function makeCamera() {
         const left = window.innerWidth / -100;
         const right = window.innerWidth / 100;
@@ -16,32 +17,46 @@ function main() {
     const renderer = new THREE.WebGL1Renderer({ canvas: canvas });
     renderer.setSize(window.innerWidth, window.innerHeight);
     const camera = makeCamera();
-    const boxGeo = new THREE.SphereGeometry(0.4, 4, 4);
+    const boxGeo = new THREE.SphereGeometry(0.2);
     const material = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: false });
     const mesh = new THREE.Mesh(boxGeo, material);
+    const controls = new TrackballControls(camera, canvas);
 
 
+    // three length because it's quadratic.
     const lineVector: THREE.Vector3[] = [];
     {
-        lineVector.push(new THREE.Vector3(-2, -2, 0))
-        lineVector.push(new THREE.Vector3(2, 2, 0))
+        lineVector.push(new THREE.Vector3(window.innerWidth / -200, 0, 0))
+        lineVector.push(new THREE.Vector3(0, window.innerHeight / 200, 0))
+        lineVector.push(new THREE.Vector3(window.innerWidth / 200, 0, 0))
         const geometry = new THREE.BufferGeometry().setFromPoints(lineVector);
         const line = new THREE.Line(geometry)
         scene.add(line)
     }
-    let t = btw_01(0.1);
-    let vec3 = lerp2points(lineVector[0], lineVector[1], t)
-    // const { x, y, z } = lineVector[0];
-    mesh.position.set(vec3.x, vec3.y, vec3.z);
+
     scene.add(mesh);
     camera.position.set(0, 0, 10);
     camera.lookAt(0, 0, 0)
+    let t = 0;
+    {
+        const drawPoints = drawLine(lineVector);
+        const geometry = new THREE.BufferGeometry().setFromPoints(drawPoints);
+        const line = new THREE.Line(geometry);
+        scene.add(line)
+    }
+
     function render() {
         requestAnimationFrame(render);
+        let inter = btw_01(t);
+        t += 0.01;
+        let vec3 = quadraticBezierCurve(lineVector, inter);
+        const { x, y, z } = vec3;
+        mesh.position.set(x, y, z)
         renderer.render(scene, camera)
-        // console.log(btw_01(t));
-        // t += 0.1;
-        // const [p1,p2] =
+        controls.update();
+        camera.updateProjectionMatrix()
+
+
     }
     render();
 }
--- src/lerp.ts
+++ src/lerp.ts
@@ -1,9 +1,12 @@
 import { Vector3 } from "three";
 
-function lerp2points(p1: Vector3, p2: Vector3, alpha: number): Vector3 {
-    let newPointX = lerp(p1.x, p2.x, alpha)
-    let newPointY = lerp(p1.y, p2.y, alpha)
-    return new Vector3(newPointX, newPointY, 0);
+function quadraticBezierCurve(p: Vector3[], t?: number) {
+    if (typeof t === "undefined") {
+        t = 0.5;
+    }
+    const x = ((1 - t) * (((1 - t) * p[0].x) + (t * p[1].x))) + (t * (((1 - t) * p[1].x) + (t * p[2].x)));
+    const y = ((1 - t) * (((1 - t) * p[0].y) + (t * p[1].y))) + (t * (((1 - t) * p[1].y) + (t * p[2].y)));
+    return new Vector3(x, y, 0);
 }
 function btw_01(n = 0) {
     if (Math.floor(n) % 2 === 0) {
@@ -12,11 +15,23 @@ function btw_01(n = 0) {
         return 1 - (n % 1)
     };
 };
+function drawLine(p: Vector3[]): Vector3[] {
+    let t = 0;
+    let arr: Vector3[] = [];
 
-function lerp(min: number, max: number, alpha: number) {
-    return ((max - min) * alpha) + min;
+    while (t < 1) {
+        const Draw = quadraticBezierCurve(p, t);
+        t += 0.01;
+        arr.push(Draw)
+    }
+    arr.push(quadraticBezierCurve(p, t));
+    return arr;
+}
+
+function lerp(p0: number, p1: number, t: number) {
+    return ((1 - t) * p0) + (t * p1);
 };
 
 lerp(200, 100, 0.5)
 
-export { btw_01, lerp2points }
+export { btw_01, quadraticBezierCurve, drawLine }
